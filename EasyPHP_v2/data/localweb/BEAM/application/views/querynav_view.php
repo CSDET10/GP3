114d25f636090146985ff67569af03c6a39f2eeb
iTiagoL <tiagoestevamlima@icloud.com>
2020-03-29 20:22:53 +0100

    Design update
    Made some updates on the design on the queries page.

--- EasyPHP_v2/data/localweb/BEAM/application/views/querynav_view.php
+++ EasyPHP_v2/data/localweb/BEAM/application/views/querynav_view.php
@@ -2,20 +2,47 @@
 <html>
 <head>
 	<meta charset="utf-8" />
+	<title>Queries</title>
 	<style>
-		h1 {text-align: center; font-family: Calibri;}
+		
+		h1 {
+			text-align: center; 
+			font-family: Helvetica Neue, Helvetica, Arial;
+		}
+
+		div {
+			background: white;
+			margin: auto;
+		}
+
+		ul {
+			margin: 0;
+			padding: 0;
+			list-style-type: none;
+		}
+
+		li {
+			display: inline-block;
+			margin-right: 20px;
+			font-family: Helvetica Neue, Helvetica, Arial;
+		}
 	</style>
+
 </head>
+
+
 <body>
 
 <h1>Queries</h1>
 <div align='center'>
 	<!--<button type="submit" onclick="location.href='<#?php echo site_url('main/query1')?>'">Total customer orders</button> -->
 	<form action=<?php echo site_url('main/query1')?>>
-		Stage Number: <input type="text" name="stageNo"><br>
-		Date: <input type="text" name="date"><br>
-		Member ID: <input type="text" name="memberID"><br>
-		<input type="submit" onclick="location.href='<?php echo site_url('main/query1')?>'">
+		<ul>
+			<li> <strong>Stage Number:</strong> <input type="text" name="stageNo"><br></li>
+			<li> <strong>Date:</strong> <input type="date" name="date"><br></li>
+			<li> <strong>Member ID:</strong> <input type="text" name="memberID"><br></li>
+		<br><input type="submit" onclick="location.href='<?php echo site_url('main/query1')?>'"></br>
+		</ul>
 	</form>
 </div>
     
