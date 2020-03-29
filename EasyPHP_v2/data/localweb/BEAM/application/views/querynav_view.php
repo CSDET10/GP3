6ec379c3e9087f2696a6ff8f14ffa7c09a6f8e28
Roberto <robertofigueiredo2000@gmail.com>
2020-03-29 20:44:08 +0100

    fix header

--- EasyPHP_v2/data/localweb/BEAM/application/views/querynav_view.php
+++ EasyPHP_v2/data/localweb/BEAM/application/views/querynav_view.php
@@ -4,28 +4,20 @@
 	<meta charset="utf-8" />
 	<title>Queries</title>
 	<style>
-		
-		h1 {
-			text-align: center; 
-			font-family: Helvetica Neue, Helvetica, Arial;
-		}
+	h1 { padding-top: 50px;text-align: center; 	font-family: Calibri; padding-top: 50px}
 
 		div {
 			background: white;
 			margin: auto;
 		}
 
-		ul {
-			margin: 0;
-			padding: 0;
-			list-style-type: none;
-		}
-
-		li {
+		#id1 {
 			display: inline-block;
 			margin-right: 20px;
+			padding-bottom: 50px;
 			font-family: Helvetica Neue, Helvetica, Arial;
 		}
+
 	</style>
 
 </head>
@@ -38,13 +30,14 @@
 	<!--<button type="submit" onclick="location.href='<#?php echo site_url('main/query1')?>'">Total customer orders</button> -->
 	<form action=<?php echo site_url('main/query1')?>>
 		<ul>
-			<li> <strong>Stage Number:</strong> <input type="text" name="stageNo"><br></li>
-			<li> <strong>Date:</strong> <input type="date" name="date"><br></li>
-			<li> <strong>Member ID:</strong> <input type="text" name="memberID"><br></li>
+			<li id ="id1"> <strong>Stage Number:</strong> <input type="text" name="stageNo"><br></li>
+			<li id ="id1"> <strong>Date:</strong> <input type="date" name="date"><br></li>
+			<li id ="id1"> <strong>Member ID:</strong> <input type="text" name="memberID"><br></li>
+
 		<br><input type="submit" onclick="location.href='<?php echo site_url('main/query1')?>'"></br>
 		</ul>
 	</form>
 </div>
-    
+
 </body>
 </html>
