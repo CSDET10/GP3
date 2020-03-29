91af53dfe2ce4459a6e15f12a8fc98398192232a
iTiagoL <tiagoestevamlima@icloud.com>
2020-03-29 22:49:24 +0100

    Queries Design Updates

--- EasyPHP_v2/data/localweb/BEAM/application/views/querynav_view.php
+++ EasyPHP_v2/data/localweb/BEAM/application/views/querynav_view.php
@@ -4,18 +4,52 @@
 	<meta charset="utf-8" />
 	<title>Queries</title>
 	<style>
-	h1 { padding-top: 50px;text-align: center; 	font-family: Calibri; padding-top: 50px}
+	h1 { padding-top: 50px;text-align: center; font-family: Calibri; padding-top: 50px}
 
 		div {
-			background: white;
+			background: #ededed;
 			margin: auto;
 		}
 
 		#id1 {
-			display: inline-block;
-			margin-right: 20px;
+			display: inline-grid;
+			text-align: center;
+			margin-right: 50px;
 			padding-bottom: 50px;
 			font-family: Helvetica Neue, Helvetica, Arial;
+			color: #646362;
+		}
+
+		#id2 {
+			background-color: #e6e6e6;
+			height: 30px;
+			color: #555;
+			font-weight: normal;
+			font-family: Verdana, Arial, sans-serif;
+  			font-size: 12px;
+			border: 1px solid #d3d3d3;
+			border-bottom-right-radius: 4px;
+			border-bottom-left-radius: 4px;
+			border-top-right-radius: 4px;
+			border-top-left-radius: 4px;
+			display: inline-block;
+    		position: relative;
+    		line-height: normal;
+    		margin-right: .1em;
+    		cursor: pointer;
+    		vertical-align: middle;
+    		text-align: center;
+    		overflow: visible;
+  	
+		}
+
+		#id3 {
+			height: 20px;
+
+		}
+
+		#id4 {
+			background: white;
 		}
 
 	</style>
@@ -29,13 +63,13 @@
 <div align='center'>
 	<!--<button type="submit" onclick="location.href='<#?php echo site_url('main/query1')?>'">Total customer orders</button> -->
 	<form action=<?php echo site_url('main/query1')?>>
-		<ul>
-			<li id ="id1"> <strong>Stage Number:</strong> <input type="text" name="stageNo"><br></li>
-			<li id ="id1"> <strong>Date:</strong> <input type="date" name="date"><br></li>
-			<li id ="id1"> <strong>Member ID:</strong> <input type="text" name="memberID"><br></li>
-
-		<br><input type="submit" onclick="location.href='<?php echo site_url('main/query1')?>'"></br>
-		</ul>
+			<div id ="id3"></div>
+			<div id ="id1"> <strong>Stage Number:</strong> <input type="text" name="stageNo"><br></div>
+			<div id ="id1"> <strong>Date:</strong> <input type="date" name="date"><br></div>
+			<div id ="id1"> <strong>Member ID:</strong> <input type="text" name="memberID"><br></div>
+</div>
+<div align='center' id="id4">
+			<br><input id ="id2" value='Submit' type="submit" onclick="location.href='<?php echo site_url('main/query1')?>'"></br>
 	</form>
 </div>
 
