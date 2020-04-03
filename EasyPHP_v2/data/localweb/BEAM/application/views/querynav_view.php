7590cdf49905f46d44b4b7a7d5aaf7685f11e02c
Roberto <robertofigueiredo2000@gmail.com>
2020-04-03 03:41:21 +0100

    update queries design

--- EasyPHP_v2/data/localweb/BEAM/application/views/querynav_view.php
+++ EasyPHP_v2/data/localweb/BEAM/application/views/querynav_view.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8" />
-	<title>Queries</title>
+	<title>Authorization System</title>
 	<style>
 	h1 { padding-top: 50px;text-align: center; font-family: Calibri; padding-top: 50px}
 
@@ -15,41 +15,45 @@
 			display: inline-grid;
 			text-align: center;
 			margin-right: 50px;
-			padding-bottom: 50px;
+			padding-bottom: 10px;
+			padding-top: 20px;
 			font-family: Helvetica Neue, Helvetica, Arial;
 			color: #646362;
 		}
 
-		#id2 {
-			background-color: #e6e6e6;
-			height: 30px;
-			color: #555;
-			font-weight: normal;
-			font-family: Verdana, Arial, sans-serif;
-  			font-size: 12px;
-			border: 1px solid #d3d3d3;
-			border-bottom-right-radius: 4px;
-			border-bottom-left-radius: 4px;
-			border-top-right-radius: 4px;
-			border-top-left-radius: 4px;
-			display: inline-block;
-    		position: relative;
-    		line-height: normal;
-    		margin-right: .1em;
-    		cursor: pointer;
-    		vertical-align: middle;
-    		text-align: center;
-    		overflow: visible;
-  	
+		input[type=submit] {
+			width: 10%;
+	  background-color: #337ab7;
+	  color: white;
+	  padding: 14px 20px;
+	  margin: 8px 0;
+	  border: none;
+	  border-radius: 4px;
+	  cursor: pointer;
+}
+
+input[type=submit]:hover {
+	background-color: #286090;
+}
+input[type=text], select{
+		  width: 100%;
+		  padding: 12px 20px;
+		  margin: 8px 0;
+		  display: inline-block;
+		  border: 1px solid #ccc;
+		  border-radius: 4px;
+		  box-sizing: border-box;
+			height: 40px;
 		}
-
-		#id3 {
-			height: 20px;
-
-		}
-
-		#id4 {
-			background: white;
+input[type=date], select{
+		  width: 100%;
+		  padding: 12px 20px;
+		  margin: 8px 0;
+		  display: inline-block;
+		  border: 1px solid #ccc;
+		  border-radius: 4px;
+		  box-sizing: border-box;
+			height: 40px;
 		}
 
 	</style>
@@ -59,17 +63,16 @@
 
 <body>
 
-<h1>Queries</h1>
+<h1>Authorization System</h1>
 <div align='center'>
 	<!--<button type="submit" onclick="location.href='<#?php echo site_url('main/query1')?>'">Total customer orders</button> -->
 	<form action=<?php echo site_url('main/query1')?>>
-			<div id ="id3"></div>
-			<div id ="id1"> <strong>Stage Number:</strong> <input type="text" name="stageNo"><br></div>
-			<div id ="id1"> <strong>Date:</strong> <input type="date" name="date"><br></div>
-			<div id ="id1"> <strong>Member ID:</strong> <input type="text" name="memberID"><br></div>
-</div>
-<div align='center' id="id4">
-			<br><input id ="id2" value='Submit' type="submit" onclick="location.href='<?php echo site_url('main/query1')?>'"></br>
+
+			<div id ="id1"> <strong>Stage Number:</strong> <input type="text" name="stageNo" placeholder = "Stage Number"><br></div>
+			<div id ="id1"> <strong>Date:</strong> <input type="date" name="date" ><br></div>
+			<div id ="id1"> <strong>Member ID:</strong> <input type="text" name="memberID" placeholder="member ID"><br></div>
+<div align='center'>
+			<br><input value='Submit' type="submit" onclick="location.href='<?php echo site_url('main/query1')?>'"></br>
 	</form>
 </div>
 
