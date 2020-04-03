31677773799d3621cd08229abda7780a75aafb52
Roberto <robertofigueiredo2000@gmail.com>
2020-04-03 03:17:57 +0100

    update table design

--- EasyPHP_v2/data/localweb/BEAM/application/views/query1_view.php
+++ EasyPHP_v2/data/localweb/BEAM/application/views/query1_view.php
@@ -3,35 +3,59 @@
 <head>
 	<meta charset="utf-8" />
 	<style>
+	body { font-family: helvetica;}
 		h1, h2 { text-align: center; font-family: Calibri; }
-		table.mytable {border-collapse: collapse;}
-		table.mytable td, th {border: 1px solid grey; padding: 5px 15px 2px 7px;}
-		th {background-color: #f2e4d5;}		
+table {
+    border-spacing: 0;
+}
+th, td {
+    border: 1px solid #ededed;
+    padding: 0.5em 1em;
+}
+th{
+	background-color: #ededed;
+}
+td{
+	background-color: #f9f9f9;
+}
+/* the first 'th' within the first 'tr' of the 'thead': */
+thead tr:first-child th:first-child {
+    border-radius: 0.3em 0 0 0;
+}
+/* the last 'th' within the first 'tr' of the 'thead': */
+thead tr:first-child th:last-child {
+    border-radius: 0 0.3em 0 0;
+}
+/* the first 'td' within the last 'tr' of the 'tbody': */
+tbody tr:last-child td:first-child {
+    border-radius: 0 0 0 0.3em;
+}
+/* the last 'td' within the last 'tr' of the 'tbody': */
+tbody tr:last-child td:last-child {
+    border-radius: 0 0 0.3em 0;
+}
+
 	</style>
 </head>
 <body>
 
-<h1>Queries</h1>
+<h1>Authorization System</h1>
 <div align='center'>
 	<!--<button type="submit" onclick="location.href='<#?php echo site_url('main/query1')?>'">Total customer orders</button>-->
 	<!--<button type="submit" onclick="location.href='<#?php echo site_url('main/query2')?>'">Ranked items by sales</button>-->
-</div>
-<h2>Total Customer Orders</h2>
-<div align='center'>
 <?php
-	
-	
+
+
 	$stageNo = $_GET["stageNo"];
 	$date = $_GET["date"];
 	$memberID = $_GET["memberID"];
-	
-	
 	$tmpl = array ('table_open' => '<table class="mytable">');
-	$this->table->set_template($tmpl); 
-	
+	$this->table->set_template($tmpl);
+
 	$this->db->query('drop table if exists temp');
 	$this->db->query('create temporary table temp as (select s.stageName, p.date, m.band from performance p join stage s on p.stageNumber = s.stageNumber join member m on p.bandName = m.band where m.memberID = "'.$memberID.'" and p.stageNumber = "'.$stageNo.'" and p.date = "'.$date.'" )');
 	$query = $this->db->query('select * from temp;');
+
 	if($query->num_rows == 0)
 	{
 		echo "DENIED";
@@ -39,10 +63,14 @@
 	else
 	{
 		echo "ALLOWED";
-		echo $this->table->generate($query);
+
+		echo $this->table->generate($query); //do we really need the table?
+
 	}
 ?>
+
+
 </div>
-	
+
 </body>
 </html>
