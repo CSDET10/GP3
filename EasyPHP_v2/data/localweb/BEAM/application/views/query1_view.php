798105bd8e7e7e567aa0d9deeab4280022080973
Roberto <robertofigueiredo2000@gmail.com>
2020-04-04 01:27:18 +0100

    table entry log system

--- EasyPHP_v2/data/localweb/BEAM/application/views/query1_view.php
+++ EasyPHP_v2/data/localweb/BEAM/application/views/query1_view.php
@@ -55,20 +55,39 @@ tbody tr:last-child td:last-child {
 	$this->db->query('drop table if exists temp');
 	$this->db->query('create temporary table temp as (select s.stageName, p.date, m.band from performance p join stage s on p.stageNumber = s.stageNumber join member m on p.bandName = m.band where m.memberID = "'.$memberID.'" and s.stageName = "'.$stageName.'" and p.date = "'.$date.'" )');
 	$query = $this->db->query('select * from temp;');
+	$capacityQuery = $this->db->query("SELECT * FROM Stage WHERE stageName = '$stageName'");
 
-
-
+	foreach ($capacityQuery->result() as $row)
+	{
+   $CurrentOccupation =  $row->quantityLive;
+	 $capacity = $row->backstageCapacity;
+	}
 
 
 	if($query->num_rows == 0)
 	{
-		echo "DENIED";
+		echo ' <script type="text/javascript">
+	            alert("You don\'t have access to this stage.");
+	            window.location.href = "/BEAM/index.php/main/querynav";
+
+	</script>';
 	}
 	else
 	{
-		echo "ALLOWED";
+		if($capacity > $CurrentOccupation){
+			$stageName = ucfirst($stageName);
+			echo "ALLOWED";
+			$this->db->query("UPDATE Stage SET quantityLive = quantityLive + 1 WHERE stageName = '$stageName'");
+			echo $this->table->generate($query); //do we really need the table?
+		}
+		else{
+			echo ' <script type="text/javascript">
+		            alert("This stage has reached maximum capacity");
+		            window.location.href = "/BEAM/index.php/main/querynav";
 
-		echo $this->table->generate($query); //do we really need the table?
+		</script>';
+		}
+
 
 	}
 ?>
